f5265ec0a4edc969424b1965abac54c2b2e9208f
rafaelrochasilva <rafaelrochasilva@gmail.com>
2014-11-26 15:56:41 -0200

    Add validation tests to Event model

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,3 +1,5 @@
 class Event < ActiveRecord::Base
 	belongs_to :user
+
+	validates_presence_of :date, :title, :description
 end
--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -9,4 +9,21 @@ describe Event do
 		expect(event).to be_valid
 	end
 
+	it "is invalid without date" do
+		event = user.events.new(title: "Lorem ipsum", description: "Lorem ipsum")
+		event.valid?
+		expect(event.errors[:date]).to include "can't be blank"
+	end
+
+	it "is invalid without title" do
+		event = user.events.new(date: DateTime.now.to_date, description: "Lorem ipsum")
+		event.valid?
+		expect(event.errors[:title]).to include "can't be blank"
+	end
+
+	it "is invalid without description" do
+		event = user.events.new(date: DateTime.now.to_date, title: "Lorem ipsum")
+		event.valid?
+		expect(event.errors[:description]).to include "can't be blank"
+	end
 end
