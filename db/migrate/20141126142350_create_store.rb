58186e4e31818c7bebb047d141a103c6daf470aa
rafaelrochasilva <rafaelrochasilva@gmail.com>
2014-11-26 12:28:01 -0200

    Add Store table

---
+++ db/migrate/20141126142350_create_store.rb
@@ -0,0 +1,14 @@
+class CreateStore < ActiveRecord::Migration
+  def change
+    create_table :stores do |t|
+    	t.belongs_to :user, null: false
+    	t.string :name, null: false
+    	t.string :address, null: false
+    	t.string :phone, null: false
+    	t.string :website
+    	t.string :picture_path
+
+    	t.timestamp
+    end
+  end
+end
