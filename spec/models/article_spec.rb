6dc5f8b5490ffaceaf3f0c9aa9b9a980c12a4939
rafaelrochasilva <rafaelrochasilva@gmail.com>
2014-11-26 12:05:06 -0200

    Add validation to description

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -1,7 +1,4 @@
 require 'rails_helper'
-include Warden::Test::Helpers
-Warden.test_mode!
-
 
 describe Article do
 	
@@ -11,5 +8,17 @@ describe Article do
 	it "is valid with a title, description" do 
 		article = user.articles.create(title: "Hi", description: "Lorem ipsum")
 		expect(article).to be_valid
+	end
+
+	it "is invalid without a title" do
+		article = user.articles.new(description: "Lorem ipsum")
+		article.valid?
+		expect(article.errors[:title]).to include("can't be blank")
+	end
+
+	it "is invalid without a description" do
+		article = user.articles.new(title: "Lorem ipsum")
+		article.valid?
+		expect(article.errors[:description]).to include("can't be blank")
 	end
 end
