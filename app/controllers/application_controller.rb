a2e747097ebf210b5cbf675dcc6818d1d516c20d
rafaelrochasilva <rafaelrochasilva@gmail.com>
2014-12-19 10:56:21 -0200

    Load the weather for every new action

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,14 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  before_action :weather
+
 
   def index
+  	@articles = Article.last(2)
+  end
+
+  def weather
   	@weather = Weather.lookup(12820853, Weather::Units::CELSIUS)
   end
 end
