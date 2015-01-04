cfbd3a5ac4e5dee2a11c0974d0f1d5548fd3c04f
rafaelrochasilva <rafaelrochasilva@gmail.com>
2015-01-04 13:25:34 -0200

    Remove table store and make a new association with promotion and user

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150104123042) do
+ActiveRecord::Schema.define(version: 20150104151815) do
 
   create_table "articles", force: true do |t|
     t.integer  "user_id",                        null: false
@@ -50,7 +50,6 @@ ActiveRecord::Schema.define(version: 20150104123042) do
   end
 
   create_table "promotions", force: true do |t|
-    t.integer  "store_id"
     t.string   "title",                              null: false
     t.string   "description",                        null: false
     t.integer  "price",                              null: false
@@ -62,17 +61,7 @@ ActiveRecord::Schema.define(version: 20150104123042) do
     t.string   "photorake_content_type"
     t.integer  "photorake_file_size"
     t.datetime "photorake_updated_at"
-  end
-
-  create_table "stores", force: true do |t|
-    t.integer  "user_id",      null: false
-    t.string   "name",         null: false
-    t.string   "address",      null: false
-    t.string   "phone",        null: false
-    t.string   "website"
-    t.string   "picture_path"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.integer  "user_id"
   end
 
   create_table "users", force: true do |t|
