0636443f2ea884b3dec46d53609a9407662974f2
rafaelrochasilva <rafaelrochasilva@gmail.com>
2015-01-04 14:09:29 -0200

    Add index page for promotion

--- spec/features/promotion_spec.rb
+++ spec/features/promotion_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 feature 'Promotion Manager' do
   scenario 'displays index promotions' do
     visit root_path
-    find('.nav-main').click_link("Promoções")
+    first('.nav-main').click_link("Promoções")
     expect(page).to have_content("Promoções da Semana")
   end
 end
