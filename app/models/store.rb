b64d6ee7b4c69530e8c16cf84f4f0f3253f1465e
rafaelrochasilva <rafaelrochasilva@gmail.com>
2015-01-04 13:30:00 -0200

    Remove store

--- app/models/store.rb
+++
@@ -1,9 +0,0 @@
-class Store < ActiveRecord::Base
-	belongs_to :user
-	has_many :promotions
-	
-	validates_presence_of :name, :address, :phone
-	validates :phone, format: { with: /\d{2}-\d{4}-\d{4}/, message: "bad format" }
-	validates :website, :allow_blank => true,
-											format: { with: /(http|https):\/\/[a-zA-Z0-9\-\#\/\_]+[\.][a-zA-Z0-9\-\.\#\/\_]+/i, message: "bad format" } 
-end
