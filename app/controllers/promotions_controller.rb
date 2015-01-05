70fe57820318d279facd2ac0741ffecf05bc45b0
rafaelrochasilva <rafaelrochasilva@gmail.com>
2015-01-05 14:16:58 -0200

    Add show, update, create, new actions

--- app/controllers/promotions_controller.rb
+++ app/controllers/promotions_controller.rb
@@ -3,5 +3,43 @@ class PromotionsController < ApplicationController
 		@promotions = Promotion.all
 	end
 
+	def show
+		@promotion = Promotion.find(params[:id])
+	end
+
+	def new
+		@promotion = Promotion.new
+	end
+
+	def create
+		@promotion = current_user.promotions.new(promo_params)
+		if @promotion.save
+			flash[:notice] = "Promoção criada com sucesso"
+			redirect_to @promotion
+		else
+			render 'new'
+		end
+
+	end
+
+	def edit
+		@promotion = Promotion.find(params[:id])
+	end
+
+	def update
+		@promotion = Promotion.find(params[:id])
+
+		if @promotion.update(promo_params)
+			redirect_to @promotion
+		else
+			flash[:notice] = "Ocorreu um erro ao fazer o update"
+			render 'edit'
+		end
+	end
+
+	private
+	def promo_params
+		params.require(:promotion).permit(:title, :description, :photo, :price)
+	end
 
 end
