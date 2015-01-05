41e6a5cd5c6c014ec5effdb908843d98aea1741d
rafaelrochasilva <rafaelrochasilva@gmail.com>
2015-01-05 14:22:14 -0200

    Remove Store association and add user

--- app/models/promotion.rb
+++ app/models/promotion.rb
@@ -1,5 +1,5 @@
 class Promotion < ActiveRecord::Base
-	belongs_to :store
+	belongs_to :user
 
 	has_attached_file :photo, :styles => { :medium => "500x500>", :thumb => "100x100>" }, :default_url => ":style/missing.png"
 	validates_attachment_content_type :photo, :content_type => /\Aimage\/.*\Z/
