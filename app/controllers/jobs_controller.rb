9a74d9b62dfe0000e7ee9b090bd92c67e39f2b89
rafaelrochasilva <rafaelrochasilva@gmail.com>
2015-01-06 18:39:32 -0200

    Create show, create, new actions

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -3,4 +3,25 @@ class JobsController < ApplicationController
 		@jobs = Job.all
 	end
 
+	def show
+		@job = Job.find(params[:id])
+	end
+
+	def new
+		@job = Job.new
+	end
+
+	def create
+		@job = current_user.jobs.new(job_params)
+		if @job.save
+			redirect_to @job
+		else
+			render 'new'
+		end
+	end
+
+	private
+		def job_params
+			params.require(:job).permit(:title, :description, :salary)
+		end
 end
