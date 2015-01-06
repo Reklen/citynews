aeba38edb4048f5357f72868a355907464c9615c
rafaelrochasilva <rafaelrochasilva@gmail.com>
2015-01-06 18:37:02 -0200

    Add tests for a new job

--- spec/features/job_spec.rb
+++ spec/features/job_spec.rb
@@ -1,12 +1,32 @@
 require 'rails_helper'
 
 feature "Job management" do
+	let(:user) { FactoryGirl.create(:user) }
+	before { login_as(user, scope: :user) }
 
 	scenario 'display all jobs' do
 	  visit root_path
-
 	  first(".nav-main").click_link("Empregos")
 
 	  expect(page).to have_content "Empregos da Semana"
+	end
+
+	scenario 'display a new job form' do
+		visit root_path
+		first(".nav-user").click_link("Empregos")
+		expect(page).to have_content "Compartilhe uma vaga de emprego"
+	end
+
+	scenario 'create a new job' do
+		job = FactoryGirl.build(:job)
+
+		visit new_job_path(job)
+		fill_in "job_title", with: job.title
+		fill_in "job_description", with: job.description
+		fill_in "job_salary", with: job.salary
+
+		click_button "Enviar"
+
+		expect(page).to have_content job.title
 	end
 end
