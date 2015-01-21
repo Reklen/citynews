bc42e7b720c1e4a4aa5d3dd57e74f944021c75fd
Rafael Rocha <rafaelrochasilva@gmail.com>
2015-01-21 17:20:27 -0200

    Add association with picture and remove paperclip

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,7 +1,6 @@
 class Event < ActiveRecord::Base
 	belongs_to :user
-	has_attached_file :photo, :styles => { :medium => "500x500>", :thumb => "100x100>" }, :default_url => ":style/missing.png"
-	validates_attachment_content_type :photo, :content_type => /\Aimage\/.*\Z/
+	has_many :pictures, as: :imageable
 
 	validates_presence_of :date, :title, :description
 end
