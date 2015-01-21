b0af6ee46229a2b1e74f9a43a73d0c8e971d798c
Rafael Rocha <rafaelrochasilva@gmail.com>
2015-01-21 17:20:02 -0200

    Remove paperclip and add polimorphic relation with picture

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,8 +1,6 @@
 class Article < ActiveRecord::Base
 	belongs_to :user
+	has_many :pictures, as: :imageable
+
 	validates_presence_of :title, :description
-
-	has_attached_file :photo, :styles => { :medium => "500x500>", :thumb => "100x100>" }, :default_url => ":style/missing.png"
-	validates_attachment_content_type :photo, :content_type => /\Aimage\/.*\Z/
-
 end
