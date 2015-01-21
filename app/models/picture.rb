b387ac3f416b841197701935116a37dbe0b7ab28
Rafael Rocha <rafaelrochasilva@gmail.com>
2015-01-21 17:19:20 -0200

    Add picture model

---
+++ app/models/picture.rb
@@ -0,0 +1,6 @@
+class Picture < ActiveRecord::Base
+	belongs_to :imageable, polymorphic: true
+
+	has_attached_file :photo, :styles => { :medium => "500x500>", :thumb => "100x100>" }, :default_url => ":style/missing.png"
+	validates_attachment_content_type :photo, :content_type => /\Aimage\/.*\Z/
+end
