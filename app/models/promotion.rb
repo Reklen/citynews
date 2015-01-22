c026f17fe6a58252aa173c8124700b671ff5756b
Rafael Rocha <rafaelrochasilva@gmail.com>
2015-01-22 13:47:35 -0200

    Add picture association

--- app/models/promotion.rb
+++ app/models/promotion.rb
@@ -1,6 +1,7 @@
 class Promotion < ActiveRecord::Base
 	belongs_to :user
-	has_many :pictures, as: :imageable
+	has_one :picture, as: :imageable
+  accepts_nested_attributes_for :picture
 	
 	validates_presence_of :title, :description, :price
 	validates :price, numericality: true
