13e6a4ab3f41632f7528aaa2a9ab9e9867daad10
rafaelrochasilva <rafaelrochasilva@gmail.com>
2015-01-25 10:00:41 -0200

    Fix the default image path

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -1,6 +1,6 @@
 class Picture < ActiveRecord::Base
 	belongs_to :imageable, polymorphic: true
 
-	has_attached_file :photo, :styles => { :medium => "500x500>", :thumb => "100x100>" }, :default_url => ":style/missing.png"
+	has_attached_file :photo, :styles => { :medium => "500x500>", :thumb => "100x100>" }, :default_url => "images/:style/missing.png"
 	validates_attachment_content_type :photo, :content_type => /\Aimage\/.*\Z/
 end
