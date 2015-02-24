2251a96b30bee65fb81978cf408a3e4dd8cf8868
Rafael Rocha <rafaelrochasilva@gmail.com>
2015-02-24 07:14:23 -0300

    Displays all user posts

--- app/controllers/user_account_controller.rb
+++ app/controllers/user_account_controller.rb
@@ -2,5 +2,9 @@ class UserAccountController < ApplicationController
   before_action :authenticate_user!
 
   def index
+    @articles = current_user.articles.all
+    @events = current_user.events.all
+    @jobs = current_user.jobs.all
+    @city_comments = current_user.city_comments.all
   end
 end
