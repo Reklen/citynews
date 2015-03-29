bfdd3d45305a9fdca4886857ee20d13d2dfbdf27
Rafael Rocha <rafaelrochasilva@gmail.com>
2015-03-28 23:23:25 -0300

    Add location relation into article

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,6 +2,8 @@ class Article < ActiveRecord::Base
   searchkick
   belongs_to :user
   has_one :picture, as: :imageable
+  has_one :location, as: :locationable
+
   accepts_nested_attributes_for :picture
 
   validates_presence_of :title, :description
