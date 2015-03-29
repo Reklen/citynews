fe8464ea42daea6f3b52debebbad06b854ced4d4
Rafael Rocha <rafaelrochasilva@gmail.com>
2015-03-28 23:22:16 -0300

    Create a seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,29 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+# user = User.new
+# user.email = 'test@example.com'
+# user.password = '123456'
+# user.password_confirmation = '123456'
+# user.name = 'John'
+# user.save!
+
+user.first
+
+30.times do
+  article = user.articles.create(
+    title: Faker::Lorem.sentence,
+    description: Faker::Lorem.paragraph(3),
+  )
+
+  location = Location.create(
+    latitude: Faker::Address.latitude,
+    longitude: Faker::Address.longitude,
+    city: Faker::Address.city,
+    country: Faker::Address.country,
+    state: Faker::Address.state
+  )
+
+  article.location = location
+end
