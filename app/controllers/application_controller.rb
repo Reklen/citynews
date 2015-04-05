18e394924d2b4d9883ecbeca0f4fbd0d449ea18d
Rafael Rocha <rafaelrochasilva@gmail.com>
2015-04-05 09:50:01 -0300

    Remove weather api

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,17 +2,10 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-  before_action :weather
-
 
   def index
     @articles = Article.last(2).reverse
-    @eventes = Event.last(2).reverse
+    @events = Event.last(2).reverse
     @city_comments = CityComment.last(4).reverse
-  end
-
-  def weather
-    #Weather.lookup(428306, Weather::Units::CELSIUS) ||
-    @weather = Weather.lookup(455827, Weather::Units::CELSIUS)
   end
 end
--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,7 +3,7 @@ class ArticlesController < ApplicationController
   before_action :set_article, only: [:edit, :update, :destroy]
 
   def index
-    render json: Article.search_by_location
+    render json: Article.search_by_location(22.9068, 43.1729)
   end
 
   def show
