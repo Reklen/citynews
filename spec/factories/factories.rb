3c1fb9f43a87a3a7d86361435e65db203f21aa11
Rafael Rocha <rafaelrochasilva@gmail.com>
2015-04-08 07:35:58 -0300

    Add more samples to factories

--- spec/factories/factories.rb
+++ spec/factories/factories.rb
@@ -4,31 +4,40 @@ FactoryGirl.define do
     password { Faker::Internet.password }
   end
 
+  factory :location do
+    latitude 22.02
+    longitude 22.02
+    city "São Paulo"
+    state "São Paulo"
+    country "Brasil"
+  end
+
   factory :article do
-  	title Faker::Lorem.sentence
-  	description Faker::Lorem.paragraph
+    title Faker::Lorem.sentence
+    description Faker::Lorem.paragraph
+    location
     user
   end
 
   factory :city_comment do
-  	title Faker::Lorem.sentence
-  	description Faker::Lorem.paragraph
-  	comment_type true
-  	user
+    title Faker::Lorem.sentence
+    description Faker::Lorem.paragraph
+    comment_type true
+    user
   end
 
   factory :event do
-		date DateTime.now.to_date
-		title Faker::Lorem.sentence
-  	description Faker::Lorem.paragraph
-  	user
+    date DateTime.now.to_date
+    title Faker::Lorem.sentence
+    description Faker::Lorem.paragraph
+    user
   end
 
   factory :promotion do
-		title Faker::Lorem.sentence
-  	description Faker::Lorem.paragraph
-  	price 100
-  	user
+    title Faker::Lorem.sentence
+    description Faker::Lorem.paragraph
+    price 100
+    user
   end
 
   factory :job do
