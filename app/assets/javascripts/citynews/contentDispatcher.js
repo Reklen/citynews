aa6b94c1a2d498f0ae902cefde3cf30674178e63
Rafael Rocha <rafaelrochasilva@gmail.com>
2015-04-25 13:57:04 -0300

    Refactory code to receive different routes

--- app/assets/javascripts/citynews/contentDispatcher.js
+++ app/assets/javascripts/citynews/contentDispatcher.js
@@ -1,33 +1,31 @@
 CityNews.ContentDispatcher = (function() {
-  function ContentDispatcher() {
+  function ContentDispatcher(path, template) {
+    this.path = path;
+    this.currentTemplate = template;
+
     this.cityMap = new CityNews.Map(this);
-    this.path = 'articles/search';//remove in the future
 
     this.$container = $('#content');
-    this.initTemplates();//remove in the future
   }
 
   var fn = ContentDispatcher.prototype;
 
-  // Remove in the future
-  fn.initTemplates = function(){
-    this.template = Handlebars.compile($('#artilces-template').html());
-  };
-
-  fn.run = function(template, path) {
-    this.template = template;
+  fn.run = function(path, template) {
     this.path = path;
+    this.currentTemplate = template;
+
     this.getContent();
   };
 
   fn.getContent = function() {
-    var cityMapCenter = this.cityMap.getMapCenter();
+    var requestedRoute = this.path + '/search',
+        cityMapCenter = this.cityMap.getMapCenter(),
         cityMapCenter_str = JSON.stringify(cityMapCenter);
 
     if(this.mapCenter != cityMapCenter_str) {
       this.mapCenter = cityMapCenter_str;
 
-      $.get(this.path, cityMapCenter)
+      $.get(requestedRoute, cityMapCenter)
       .done($.proxy(this.renderContainer, this, cityMapCenter.distance.toString()));
     }
   };
@@ -44,7 +42,7 @@ CityNews.ContentDispatcher = (function() {
   };
 
   fn.appendContainer = function(distance, data) {
-    this.$container.append(this.template(data));
+    this.$container.append(this.currentTemplate(data));
     this.$container.find('[data-title]').text(distance+' km');
   };
 
