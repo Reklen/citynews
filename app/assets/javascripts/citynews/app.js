0cb37af8d489c478083b05178bfc8d37c6c38dd9
Rafael Rocha <rafaelrochasilva@gmail.com>
2015-04-25 13:58:55 -0300

    Applies click event into menu to display different contents

--- app/assets/javascripts/citynews/app.js
+++ app/assets/javascripts/citynews/app.js
@@ -2,23 +2,34 @@ CityNews.App = (function() {
   'use-strict';
 
   function App() {
-    // this.initTemplates();
-    this.ROUTES = {
-      // '#article-template': this.contentDispatcher.setTemplate(template, path);
-    };
+    this.$mainMenu = $('[data-main-menu]');
+    new CityNews.ShareModal($('[data-menu-share]'));
+
+    this.templates = this.initTemplates();
+
+    this.contentDispatcher = new CityNews.ContentDispatcher('articles', this.templates['articles']);
+    this.addEventListeners();
   }
 
   var fn = App.prototype;
 
-  fn.initTemplates = function() {
-    this.articlesTemplate = Handlebars.compile($('#artilces-template').html());
+  fn.addEventListeners = function() {
+    this.$mainMenu.on('click', '[data-menu-route]', this.displayRouteContent.bind(this));
   };
 
-  fn.run = function() {
-    console.info('=> Running the CityNews app');
-    this.contentDispatcher = new CityNews.ContentDispatcher(this.cityMap);
+  fn.initTemplates = function() {
+    return {
+      'articles': Handlebars.compile($('#artilces-template').html())
+    };
+  };
 
-    new CityNews.ShareModal($('[data-menu-share]'));
+  fn.displayRouteContent = function(e) {
+    var link = $(e.currentTarget),
+        path = link.data('menu-route');
+
+    console.log("CityNews is running the route: ", path);
+    //add in the header a new route
+    // this.contentDispatcher.run(path, this.templates[path]);
   };
 
   return App;
