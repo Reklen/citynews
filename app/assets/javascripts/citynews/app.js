88e12d9b7f3eeb501686aafb57c35491b65eb382
Rafael Rocha <rafaelrochasilva@gmail.com>
2015-04-26 16:28:13 -0300

    Refactor app class

--- app/assets/javascripts/citynews/app.js
+++ app/assets/javascripts/citynews/app.js
@@ -2,16 +2,24 @@ CityNews.App = (function() {
   'use-strict';
 
   function App() {
+<<<<<<< HEAD
     var currentPath = 'articles', //window.location.pathname;
         currentDataMenu = "[data-menu-route='"+currentPath+"']";
 
     this.$mainMenu = $('[data-main-menu]');
     this.setNewRoute(currentPath, this.$mainMenu.find(currentDataMenu));
+=======
+    this.$mainMenu = $('[data-main-menu]');
+    new CityNews.ShareModal($('[data-menu-share]'));
+>>>>>>> parent of 75c8fd5... Add a routes handle with JS
 
     this.templates = this.initTemplates();
-    this.contentDispatcher = new CityNews.ContentDispatcher(currentPath, this.templates[currentPath]);
 
+<<<<<<< HEAD
     new CityNews.FormBuilder();
+=======
+    this.contentDispatcher = new CityNews.ContentDispatcher('articles', this.templates['articles']);
+>>>>>>> parent of 75c8fd5... Add a routes handle with JS
     this.addEventListeners();
   }
 
@@ -23,17 +31,21 @@ CityNews.App = (function() {
 
   fn.initTemplates = function() {
     return {
+<<<<<<< HEAD
       '/': Handlebars.compile($('#artilces-template').html()),
       'articles': Handlebars.compile($('#artilces-template').html()),
       'events': Handlebars.compile($('#artilces-template').html())
+=======
+      'articles': Handlebars.compile($('#artilces-template').html())
+>>>>>>> parent of 75c8fd5... Add a routes handle with JS
     };
   };
 
   fn.displayRouteContent = function(e) {
-    e.preventDefault();
     var link = $(e.currentTarget),
         path = link.data('menu-route');
 
+<<<<<<< HEAD
     this.setNewRoute(path, link);
     this.contentDispatcher.run(path, this.templates[path]);
   };
@@ -44,6 +56,11 @@ CityNews.App = (function() {
       this.$mainMenu.find('a').css('text-decoration', 'none');
       link.css('text-decoration', 'underline');
     }
+=======
+    console.log("CityNews is running the route: ", path);
+    //add in the header a new route
+    // this.contentDispatcher.run(path, this.templates[path]);
+>>>>>>> parent of 75c8fd5... Add a routes handle with JS
   };
 
   return App;
